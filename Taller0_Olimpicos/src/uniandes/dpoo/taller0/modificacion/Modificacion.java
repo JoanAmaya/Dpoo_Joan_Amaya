0443c663bc1607974f3f89203f7f39e7e0482ff3
JoanAmaya <98501917+JoanAmaya@users.noreply.github.com>
2023-01-31 20:36:25 -0500

    Entrega dpoo

--- Taller0_Olimpicos/src/uniandes/dpoo/taller0/modificacion/Modificacion.java
+++ Taller0_Olimpicos/src/uniandes/dpoo/taller0/modificacion/Modificacion.java
@@ -10,6 +10,7 @@ public class Modificacion
 {
 	public static void main(String[] args) throws FileNotFoundException, IOException
 	{
+		System.out.println("Hello, World!");
 		CalculadoraEstadisticas calc = LoaderOlimpicos.cargarArchivo("./data/atletas.csv");
 		System.out.println(calc.paisConMasMedallistas());
 	}
